b632875b176a9a4a670befe9771386a1243af312
ADMIN <ADMIN@LAB1-MAY29>
2024-06-17 10:06:28 +0700

    Add project files.

---
+++ CSharp_Giaiphuongtrinhbac2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace CSharp_Giaiphuongtrinhbac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblkqa = new System.Windows.Forms.Label();
+            this.lblkqc = new System.Windows.Forms.Label();
+            this.lblkqb = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.btngiai = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grbkq = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(58, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ỨNG DỤNG GIẢI PHƯƠNG TRÌNH BẬC 2 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(228, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ax2+bx+c=0";
+            // 
+            // lblkqa
+            // 
+            this.lblkqa.AutoSize = true;
+            this.lblkqa.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblkqa.Location = new System.Drawing.Point(12, 165);
+            this.lblkqa.Name = "lblkqa";
+            this.lblkqa.Size = new System.Drawing.Size(89, 29);
+            this.lblkqa.TabIndex = 2;
+            this.lblkqa.Text = "Nhập a";
+            // 
+            // lblkqc
+            // 
+            this.lblkqc.AutoSize = true;
+            this.lblkqc.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblkqc.Location = new System.Drawing.Point(12, 296);
+            this.lblkqc.Name = "lblkqc";
+            this.lblkqc.Size = new System.Drawing.Size(87, 29);
+            this.lblkqc.TabIndex = 3;
+            this.lblkqc.Text = "Nhập c";
+            // 
+            // lblkqb
+            // 
+            this.lblkqb.AutoSize = true;
+            this.lblkqb.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblkqb.Location = new System.Drawing.Point(12, 228);
+            this.lblkqb.Name = "lblkqb";
+            this.lblkqb.Size = new System.Drawing.Size(89, 29);
+            this.lblkqb.TabIndex = 3;
+            this.lblkqb.Text = "Nhập b";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(129, 165);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(204, 36);
+            this.txta.TabIndex = 4;
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.Location = new System.Drawing.Point(129, 225);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(204, 36);
+            this.txtb.TabIndex = 5;
+            // 
+            // txtc
+            // 
+            this.txtc.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtc.Location = new System.Drawing.Point(129, 284);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(204, 36);
+            this.txtc.TabIndex = 6;
+            // 
+            // btngiai
+            // 
+            this.btngiai.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngiai.Location = new System.Drawing.Point(326, 335);
+            this.btngiai.Name = "btngiai";
+            this.btngiai.Size = new System.Drawing.Size(227, 66);
+            this.btngiai.TabIndex = 7;
+            this.btngiai.Text = "Giải phương trình";
+            this.btngiai.UseVisualStyleBackColor = true;
+            this.btngiai.Click += new System.EventHandler(this.btngiai_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::CSharp_Giaiphuongtrinhbac2.Properties.Resources.cheems_thinking_suy_nghi_loading;
+            this.pictureBox1.InitialImage = global::CSharp_Giaiphuongtrinhbac2.Properties.Resources.cheems_thinking_suy_nghi_loading;
+            this.pictureBox1.Location = new System.Drawing.Point(1186, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(399, 342);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // grbkq
+            // 
+            this.grbkq.BackColor = System.Drawing.Color.Snow;
+            this.grbkq.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbkq.Location = new System.Drawing.Point(30, 407);
+            this.grbkq.Name = "grbkq";
+            this.grbkq.Size = new System.Drawing.Size(810, 223);
+            this.grbkq.TabIndex = 9;
+            this.grbkq.TabStop = false;
+            this.grbkq.Text = "                                                                                 " +
+    "                                                                                " +
+    "                            ";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(377, 165);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(357, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(377, 225);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(357, 45);
+            this.trackBar2.TabIndex = 11;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(377, 284);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(357, 45);
+            this.trackBar3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1588, 647);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.grbkq);
+            this.Controls.Add(this.btngiai);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.lblkqb);
+            this.Controls.Add(this.lblkqc);
+            this.Controls.Add(this.lblkqa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblkqa;
+        private System.Windows.Forms.Label lblkqc;
+        private System.Windows.Forms.Label lblkqb;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Button btngiai;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox grbkq;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+    }
+}
+
---
+++ CSharp_Giaiphuongtrinhbac2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Giaiphuongtrinhbac2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btngiai_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a=Convert.ToInt32(txta.Text);
+            b=Convert.ToInt32(txtb.Text);
+            c=Convert.ToInt32(txtc.Text);
+            double delta, x1, x2;
+            if (a == 0)
+                if (b == 0)
+                    if (c != 0)
+                    {grbkq.Text = "Phương trình vô nghiệm";
+
+                    }
+            else { grbkq.Text="Phương trình có vô số nghiệm";
+                    }
+                
+                else
+                {
+                    x1 = x2 = -c / b;
+                    grbkq.Text = "Phương trình có nghiệm x = " + x1;
+                }
+            
+            else
+            {
+                delta = b * b - 4 * a * c;
+                if (delta < 0)
+                {
+                    grbkq.Text = "Phương trình vô nghiệm";
+                }
+                else if (delta == 0)
+                {
+                    x1 = x2 = -b / (2 * a);
+                    grbkq.Text = "Phương trình có nghiệm kép x1 = x2 = " + x1;
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    grbkq.Text = "Phương trình có 2 nghiệm phân biệt " + " \nx1 = " + x1 + " \nx2 = " + x2;
+                }
+            }
+                         
+            
+                        
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
